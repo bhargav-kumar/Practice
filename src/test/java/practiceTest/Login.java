b9141256603c74a0221f8d74218342901d88797f
bhargav-kumar03 <ch.bhargavkumar500@gmail.com>
2018-07-28 20:05:25 +0530

    practice code for jenkins

---
+++ src/test/java/practiceTest/Login.java
@@ -0,0 +1,55 @@
+package practiceTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "D:\\workspace\\Basics\\lib\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	}
+	
+	@Test(priority=1)
+	public void test() {
+	
+		driver.get("https:www.amazon.in");
+		
+
+	}
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

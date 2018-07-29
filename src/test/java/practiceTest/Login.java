a4e5dc6d70d4a11381ae3a1594b2fb8233e00faa
bhargav-kumar03 <ch.bhargavkumar500@gmail.com>
2018-07-29 15:50:05 +0530

    added extent report

--- src/test/java/practiceTest/Login.java
+++ src/test/java/practiceTest/Login.java
@@ -1,16 +1,58 @@
 package practiceTest;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class Login {
-	
+
 	WebDriver driver;
+	ExtentReports extent;
+	ExtentTest extentTest;
+
+	@BeforeTest
+	public void setExtent() {
+		extent = new ExtentReports(System.getProperty("user.dir") + "\\test-output\\ExtentReport.html", true);
+		extent.addSystemInfo("Host Name", "Windows");
+		extent.addSystemInfo("User Name", "WorldPeace");
+		extent.addSystemInfo("Environment", "QA");
+	}
+
+	@AfterTest
+	public void endReport() {
+		extent.flush();
+		extent.close();
+	}
+
+	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
+		String dateFormat = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		TakesScreenshot ts = ((TakesScreenshot) driver);
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		String destination = System.getProperty("user.dir")+"\\FailedTestScreenshot\\"+screenshotName+dateFormat+".png";
+		File finalDestination = new File(destination);
+		FileUtils.copyFile(src, finalDestination);
+		return destination;
+
+	}
 
 	@BeforeMethod
 	public void setUp() {
@@ -18,38 +60,38 @@ public class Login {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+		driver.get("https:www.google.com");
 	}
-	
-	@Test(priority=1)
-	public void test() {
-	
-		driver.get("https:www.amazon.in");
-		
 
+	@Test(priority = 1)
+	public void openBrowser() {
+		extentTest = extent.startTest("openBrowser");
+		driver.navigate().to("https://www.yahoo.com");
 	}
+	@Test(priority=2)
+	public void titleTest() {
+		extentTest = extent.startTest("titleTest");
+		String actual = driver.getTitle();
+		Assert.assertEquals(actual, "google1234");
+	}
+
 	@AfterMethod
-	public void tearDown() {
+	public void tearDown(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS " + result.getName());
+			extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS " + result.getThrowable());
+
+			String screenshotPath = Login.getScreenshot(driver, result.getName());
+			extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath));
+		} else if (result.getStatus() == ITestResult.SKIP) {
+			extentTest.log(LogStatus.SKIP, "TEST CASE SKIPPED IS " + result.getName());
+		} else if(result.getStatus()==ITestResult.SUCCESS) {
+			extentTest.log(LogStatus.PASS, "TEST CASE PASSED IS " + result.getName());
+		}
+
+		extent.endTest(extentTest);
+
 		driver.quit();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
